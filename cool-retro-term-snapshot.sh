262ef657048b40b6704778e5cdaa988c8a787698
kuboosoft <kuboode@gmail.com>
2015-01-28 18:53:57 -0600

    Update cool-retro-term-snapshot.sh

--- cool-retro-term-snapshot.sh
+++ cool-retro-term-snapshot.sh
@@ -24,4 +24,3 @@ git checkout ${branch}
 tag=$(git rev-list HEAD -n 1 | cut -c 1-7)
 version=1.0.0
 git archive --prefix="${name}-${version}/" --format=tar master > "$pwd"/${name}-${version}-${date}-${tag}.tar
-  
