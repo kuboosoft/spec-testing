afa1e61a45ed005979668adf4211f5f261859b55
kuboosoft <kuboode@gmail.com>
2015-02-07 18:58:15 -0600

    Update pencil2d-snapshot.sh

--- pencil2d-snapshot.sh
+++ pencil2d-snapshot.sh
@@ -22,6 +22,6 @@ git clone https://github.com/pencil2d/${package}.git
 cd ${package}/
 git checkout ${branch}
 tag=$(git rev-list HEAD -n 1 | cut -c 1-7)
-version=0.5.4
+version=0.5.5
 git archive --prefix="${name}-${version}/" --format=tar master > "$pwd"/${name}-${version}-${date}-${tag}.tar
   
